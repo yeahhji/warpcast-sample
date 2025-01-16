3807abbe7bad58834d2b07421eee1dd961657d18
haian <ada.lovelace@kakao.com>
2025-01-16 22:39:04 +0900

    refactor: Remove unused frame metadata and clean up page.tsx

--- app/pinkwave-web/src/app/page.tsx
+++ app/pinkwave-web/src/app/page.tsx
@@ -1,33 +1,3 @@
-import { getFrameMetadata } from '@coinbase/onchainkit/frame';
-import type { Metadata } from 'next';
-
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      action: 'link',
-      label: '🎀 Preview PinkWave 🎀',
-      target: 'https://maemesoft.notion.site/PinkWave-Project-2222780bdf6c4af6b0d24945dc04de1c',
-    },
-  ],
-  image: {
-    aspectRatio: '1:1',
-    src: `http://static.socialdev.club/pinkwave/teaser.png`,
-  },
-});
-
-export const metadata: Metadata = {
-  description: 'LFG',
-  openGraph: {
-    description: 'LFG',
-    images: [`http://static.socialdev.club/pinkwave/teaser.png`],
-    title: 'pinkwave',
-  },
-  other: {
-    ...frameMetadata,
-  },
-  title: 'pinkwave',
-};
-
 export default function Page() {
   return (
     <>
