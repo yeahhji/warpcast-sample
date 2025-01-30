9d3fc6e6882b7ba4b1b5445af164248b49926a0f
kaina <keyvia@naver.com>
2025-01-30 12:38:32 +0900

    change hun.ver

--- app/pinkwave-web/src/app/roulette/page.tsx
+++ app/pinkwave-web/src/app/roulette/page.tsx
@@ -4,13 +4,13 @@ import { NEXT_PUBLIC_URL } from '../config';
 export const metadata: Metadata = {
   other: {
     'fc:frame': 'vNext',
-    'fc:frame:button:1': 'Shoot',
+    'fc:frame:button:1': 'Go',
     'fc:frame:button:1:action': 'post',
     'fc:frame:button:1:post_url': `${NEXT_PUBLIC_URL}/api/roulette/shoot`,
-    'fc:frame:button:2': 'Stop',
+    'fc:frame:button:2': 'Ice',
     'fc:frame:button:2:action': 'post',
     'fc:frame:button:2:post_url': `${NEXT_PUBLIC_URL}/api/roulette/stop`,
-    'fc:frame:image': `${NEXT_PUBLIC_URL}/roulette/hey.png`,
+    'fc:frame:image': `${NEXT_PUBLIC_URL}/roulette/hun.png`,
     'fc:frame:image:aspect_ratio': '1:1',
   },
   title: 'roulette',
