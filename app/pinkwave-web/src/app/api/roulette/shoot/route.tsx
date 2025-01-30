dfc972b91dc15aa7da9e33f40b3e1a6e53013e56
kaina <keyvia@naver.com>
2025-01-30 12:59:14 +0900

    add button

--- app/pinkwave-web/src/app/api/roulette/shoot/route.tsx
+++ app/pinkwave-web/src/app/api/roulette/shoot/route.tsx
@@ -5,6 +5,13 @@ import { getFrameHtmlResponse } from '../../../../utils/getFrameHtmlResponse';
 async function getResponse(): Promise<NextResponse> {
   return new NextResponse(
     getFrameHtmlResponse({
+      buttons: [
+        {
+          action: 'post',
+          label: '🔄 다시 시작하기',
+          target: `${NEXT_PUBLIC_URL}/api/roulette/reset`,
+        },
+      ],
       image: {
         aspectRatio: '1:1',
         src: `${NEXT_PUBLIC_URL}/roulette/died.png`,
